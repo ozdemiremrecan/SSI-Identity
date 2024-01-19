5391d18f81cd99ea3c66b1feddb0f4b253abe489
Emre Can Özdemir <93479988+ozdemiremrecan@users.noreply.github.com>
2024-01-19 14:02:59 +0300

    Update Project.sol

--- Project.sol
+++ Project.sol
@@ -2,20 +2,21 @@
 pragma solidity >=0.7.0 <0.9.0;
 contract Project{
     struct Issuer{
-        string DId;
+        uint DId;
         string Name;
         bytes32 Ukey;
         bytes32 Rkey;
     }
     struct Credential{
-        string DId;
-        string IssuerId;
-        string Type;
+        uint DId;
+        uint walletId;
+        string[] Type;
         uint Issued;
+        bytes32 Ukey;
+        bytes32 Rkey;
     }
     struct Wallet{
         uint DId;
-        address x;
         bytes32 Ukey;
         bytes32 Rkey;
         string Type;
@@ -24,38 +25,49 @@ contract Project{
     Credential[]private credentials;
     Issuer[]private issuers;
     error NotFound();
-    event checkedWallet(string DId,string message);
-    mapping(uint=>string)private typeToWallet;
-    function typeBuilder()private{
-        typeToWallet[0]="User";
-        typeToWallet[1]="Issuer";
-        typeToWallet[2]="Verifier";
+    mapping(address=>Wallet) walletMapping;
+    mapping(uint=>Credential) credentialMapping;
+    mapping(uint=>Issuer) issuerMapping;
+    uint walletI=0;
+    uint credentialI=0;
+    uint issuerI=0;
+
+    event checkedWallet(uint DId,string message);
+    mapping(string=>string) typeToWallet;
+    function typeBuilder()private {
+        typeToWallet["0"]="User";
+        typeToWallet["1"]="Issuer";
+        typeToWallet["2"]="Verifier";
     }
-    mapping(address => string[2] ) private SetAddressToCredential;
-    function createWallet(uint typeNo)external{
+    mapping(address => bytes32[2] ) private SetAddressToCredential;
+    mapping(address => bytes32[2])private savingWallet;
+    function createWallet(string memory typeNo,string[] memory information)external{
         typeBuilder();
-        Wallet storage wallet=wallets.push();
-        wallet.DId=uint(keccak256(abi.encode(block.difficulty, block.timestamp)));
-        wallet.x=msg.sender;
-        wallet.Ukey=keccak256(abi.encode());
-        wallet.Rkey=keccak256(abi.encode());
-        wallet.Type=typeToWallet[typeNo];
+        uint DId=uint(keccak256(abi.encode(block.difficulty, block.timestamp)));
+        bytes32 Ukey=keccak256(abi.encode());
+        bytes32 Rkey=keccak256(abi.encode());
+        string memory Type=typeToWallet[typeNo];
+        uint credentialDId=uint(keccak256(abi.encode(block.timestamp)));
+        string[] memory credentialType=information;
+        uint Issued=block.timestamp;
+        Credential memory credential=Credential(credentialDId,DId,credentialType,Issued,Ukey,Rkey);
+        walletMapping[msg.sender]=Wallet(DId,Ukey,Rkey,Type);
+        credentialMapping[credentialDId]=credential;
+        SetAddressToCredential[msg.sender]=[keccak256(abi.encode(credentialDId)),Ukey];
     }
-    function setDummyData()external{
-        issuers.push(Issuer("issuerDid","Edevlet","1","2")); 
-        credentials.push(Credential("credentialDid",issuers[0].DId,"1",block.timestamp));//
-        
+    function writeBlockchain()public{
+        require(checkWallet(walletMapping[msg.sender].DId),"Invalid Wallet");
+        savingWallet[msg.sender]=[keccak256(abi.encode(walletMapping[msg.sender].DId)),walletMapping[msg.sender].Ukey];
     }
-    function checkWallet(string memory Id)private returns(bool) {   
-        for(uint i=0;i<wallets.length;i++){
-            if(keccak256(abi.encode(wallets[i].DId))==keccak256(abi.encode(Id))){
-                emit checkedWallet(Id, "Wallet has been checked.");
-                return true;
-            }
-        }
-        return false;
+    function getWallet()external view returns(bytes32[2] memory){
+        return savingWallet[msg.sender];
     }
-    function checkIssuer(string memory IssuerDId)private view returns(bytes32){
+    function checkWallet(uint Id)public pure returns(bool) {   
+        require(keccak256(abi.encode(Id))!= keccak256(abi.encode(0)),"Invalid Id");
+        return true;
+    }
+    function checkIssuer(bytes32 IssuerDId)private view returns(bytes32){
+        require(IssuerDId == keccak256(abi.encode(issuerMapping[uint(IssuerDId)])));
         for(uint i=0;i<issuers.length;i++){
             if(keccak256(abi.encode(IssuerDId)) == keccak256(abi.encode(issuers[i].DId))){
                 return issuers[i].Ukey;
@@ -63,17 +75,16 @@ contract Project{
         }
         return "";
     }
-    function ReturnCredential(string memory DId)private returns(string[2] memory){
+    function ReturnCredential(uint DId)private view returns(bytes32[2] memory){
         require(checkWallet(DId) == true,"Invalid Wallet");
-        SetAddressToCredential[msg.sender]=[issuers[0].DId,credentials[0].DId];
         return SetAddressToCredential[msg.sender];
     }
-    function DecryptObject(string memory EncryptedId)private pure returns(string memory){
+    function DecryptObject(bytes32 EncryptedId)private pure returns(bytes32){
         return EncryptedId;  //normalde byte dönmesi gerekiyor. Crypt fonksiyonları yazılana kadar böyle kalacak.
     }
-    function Verification(string memory WalletDId)public returns(Credential memory){
+    function Verification(uint WalletDId)public view returns(Credential memory){
         require(checkIssuer(ReturnCredential(WalletDId)[0]) != keccak256(abi.encode("")),"Invalid Issuer");
-        string memory value=DecryptObject(ReturnCredential(WalletDId)[1]);
+        bytes32 value=DecryptObject(ReturnCredential(WalletDId)[1]);
         for(uint i=0;i<credentials.length;i++){
             if(keccak256(abi.encode(value))==keccak256(abi.encode(credentials[i].DId))){
                 return credentials[i];
